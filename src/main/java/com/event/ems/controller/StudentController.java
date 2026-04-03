7c98fd2ad61ab82e08b67044b7d603439fe31327
maleeshachathuranga <maleesha16chathuranga@gmail.com>
2026-04-03 13:51:27 +0530

    Add Organizer and Student controllers with corresponding services and repositories

---
+++ src/main/java/com/event/ems/controller/StudentController.java
@@ -0,0 +1,25 @@
+package com.event.ems.controller;
+
+import com.event.ems.dto.ApiResponse;
+import com.event.ems.model.StudentModel;
+import com.event.ems.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping
+    public ApiResponse<List<StudentModel>> getStudentsCount() {
+        return studentService.getStudentsCount();
+    }
+}
+
