3a75b1e38db9e8c3492c98e9e09c7621b09e5a25
Nimhara Chalana <162380399+nimharachalana@users.noreply.github.com>
2026-04-22 15:02:08 +0530

    Add Repositories for Student and Registration

--- src/main/java/com/event/ems/repo/RegistrationRepo.java
+++ src/main/java/com/event/ems/repo/RegistrationRepo.java
@@ -2,6 +2,7 @@ package com.event.ems.repo;
 
 import com.event.ems.model.RegitrationStatus;
 import com.event.ems.model.RegistrationModel;
+import com.event.ems.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public interface RegistrationRepo extends JpaRepository <RegistrationModel,Long>
 	boolean existsByEvent_Id(Long eventId);
 
 	List<RegistrationModel> findByEvent_IdAndStatus(Long eventId, RegitrationStatus status);
+    List<RegistrationModel> findByUser(UserModel user);
 
 	void deleteByEvent_Id(Long eventId);
 
